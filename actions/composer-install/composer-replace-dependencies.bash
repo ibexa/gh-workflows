7850936d460c68d2ae3af8a8f50646bcc91ce469
Andrew Longosz <alongosz@users.noreply.github.com>
2025-10-02 15:12:01 +0200

    [GHA][Composer Install] Implemented handling of `shouldBeAddedAsVCS`

--- actions/composer-install/composer-replace-dependencies.bash
+++ actions/composer-install/composer-replace-dependencies.bash
@@ -9,6 +9,7 @@ composer_file=composer.json
 while IFS= read -r package; do
     name=$(echo "$package" | jq -r '.package')
     requirement=$(echo "$package" | jq -r '.requirement')
+    should_be_added_as_vcs=$(echo "$package" | jq -r '.shouldBeAddedAsVCS')
 
     if jq -e ".require | has(\"$name\")" "$composer_file" >/dev/null
     then
@@ -18,5 +19,14 @@ while IFS= read -r package; do
     then
         composer require --dev --no-update "$name":"$requirement"
         echo "Updated '$name' dev package requirement to '$requirement'"
+    else
+        # Skip further processing for a dependency which is not a part of currently processed composer.json
+        continue
+    fi
+
+    if [[ $should_be_added_as_vcs == "true" ]] ; then
+        repository_url=$(echo "$package" | jq -r '.repositoryUrl')
+        echo ">> Configuring VCS repository $repository_url for $name"
+        composer config repositories."$(uuidgen)" vcs "$repository_url"
     fi
 done < <(jq -c '.packages[]' "$dependencies_json")
